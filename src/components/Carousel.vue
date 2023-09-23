c54c130ee01f369a879703a85d5508683f1e8097
Masaki Kleinkopf <masaki.kleinkopf@gmail.com>
2023-09-23 15:15:54 -0700

    initial commit

---
+++ src/components/Carousel.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="embla" ref="emblaNode">
+    <div class="embla__container">
+      {{ emblaNode }}
+      <div class="embla__slide">slide</div>
+      <div class="embla__slide">slide</div>
+      <div class="embla__slide">slide</div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import emblaCarouselVue from 'embla-carousel-vue'
+const [emblaNode] = emblaCarouselVue()
+</script>
---
+++ src/components/Project.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="project-container">
+    <h3 class="title">{{ isDropdown }}</h3>
+    <button @click="handleClick">open dropdown</button>
+    <div v-if="isDropdown" class='dropdown'>
+      <Carousel />
+    </div>
+  </div>
+</template>
+
+<script setup>
+import Carousel from './Carousel.vue'
+import { ref } from 'vue'
+const isDropdown = ref(false)
+const handleClick = () => {
+  isDropdown.value = !isDropdown.value
+}
+</script>
+
+<style>
+.image {
+  height: 200px;
+  width:370px;
+}
+</style>
