16568bc4a50c4b83bb913b19077b3470107183d9
Masaki Kleinkopf <masaki.kleinkopf@gmail.com>
2023-09-24 15:18:54 -0700

    add styles and carousel

--- src/components/Carousel.vue
+++ src/components/Carousel.vue
@@ -1,8 +1,7 @@
 <template>
   <div class="embla" ref="emblaNode">
     <div class="embla__container">
-      {{ emblaNode }}
-      <div class="embla__slide">slide</div>
+      <div class="embla__slide"><img src="public/livenation-1.png"></div>
       <div class="embla__slide">slide</div>
       <div class="embla__slide">slide</div>
     </div>
@@ -11,5 +10,23 @@
 
 <script setup>
 import emblaCarouselVue from 'embla-carousel-vue'
-const [emblaNode] = emblaCarouselVue()
+const [emblaNode] = emblaCarouselVue({loop: true})
 </script>
+
+<style>
+.embla {
+    overflow: hidden;
+  }
+.embla__container {
+  display: flex;
+}
+.embla__slide {
+  flex: 0 0 100%;
+  min-width: 0;
+}
+
+img {
+  width:600px;
+  height:400px;
+}
+</style>
--- src/components/Project.vue
+++ src/components/Project.vue
@@ -1,9 +1,11 @@
 <template>
   <div class="project-container">
-    <h3 class="title">{{ isDropdown }}</h3>
-    <button @click="handleClick">open dropdown</button>
-    <div v-if="isDropdown" class='dropdown'>
-      <Carousel />
+    <div class="project-preview">
+      <h3 class="title">Project Title</h3>
+      <button class="dropdown-button" @click="handleClick">open dropdown</button>
+    </div>
+    <div v-show="isDropdown" class='dropdown' :class="isDropdown && 'open'">
+      <Carousel/>
     </div>
   </div>
 </template>
@@ -18,8 +20,24 @@ const handleClick = () => {
 </script>
 
 <style>
+.project-container {
+  border: solid 1px;
+  border-radius: 50px;
+  padding: 0 40px;
+}
 .image {
   height: 200px;
   width:370px;
+}
+.project-preview {
+  display: flex;
+  justify-content: space-between;
+}
+.dropdown {
+  height: 0;
+  transition: all 0.5s ease-in-out;
+}
+.open {
+  height: auto;
 }
 </style>
