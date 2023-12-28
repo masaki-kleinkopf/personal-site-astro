960001b94947f0f5d4ecf8dc6d0feaa5eb82bf00
Masaki Kleinkopf <masaki.kleinkopf@gmail.com>
2023-12-28 11:09:49 -0800

    add dropdown svg

--- src/components/Project.vue
+++ src/components/Project.vue
@@ -2,12 +2,19 @@
   <div class="project-container">
     <div class="project-preview">
       <h3 class="title">{{ title }}</h3>
-      <button class="dropdown-button" @click="handleClick">open dropdown</button>
+      <button :class="isDropdown && 'arrow-open'" class="dropdown-button" @click="handleClick">
+        <svg fill="#000000" height="30px" width="30px" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 330 330" xml:space="preserve">
+          <path id="XMLID_222_" d="M250.606,154.389l-150-149.996c-5.857-5.858-15.355-5.858-21.213,0.001
+            c-5.857,5.858-5.857,15.355,0.001,21.213l139.393,139.39L79.393,304.394c-5.857,5.858-5.857,15.355,0.001,21.213
+            C82.322,328.536,86.161,330,90,330s7.678-1.464,10.607-4.394l149.999-150.004c2.814-2.813,4.394-6.628,4.394-10.606
+            C255,161.018,253.42,157.202,250.606,154.389z"/>
+        </svg>
+      </button>
     </div>
     <div class='dropdown' :class="isDropdown && 'open'">
       <p class="caption">{{ selectedCaption }}</p>
       <MasonryGrid :images="images" @image-click="handleImageClick" />
-      <Carousel v-if="isModal" @set-scroll-index="setScrollIndex" :start-index="scrollIndex" :images="images"/>
+      <Carousel v-if="isModal" :is-modal="true" @set-scroll-index="setScrollIndex" @close-modal="() => isModal = false" :start-index="scrollIndex" :images="images"/>
     </div>
   </div>
 </template>
@@ -40,11 +47,12 @@ const images = computed(() => props.project?.photos)
 
 </script>
 
-<style>
+<style lang="scss">
 .project-container {
-  border: solid 1px;
-  border-radius: 50px;
-  padding: 0 40px;
+  border-top: solid 1px;
+  &:last-child {
+    border-bottom: solid 1px;
+  }
 }
 .image {
   height: 200px;
@@ -53,7 +61,12 @@ const images = computed(() => props.project?.photos)
 .project-preview {
   display: flex;
   justify-content: space-between;
-  height: 70px;
+  height: 100px;
+  .title {
+    display: flex;
+    align-items: center;
+    padding-left: 20px;
+  }
 }
 .dropdown {
   max-height: 0;
@@ -61,12 +74,22 @@ const images = computed(() => props.project?.photos)
   transition: opacity .25s ease-in-out, max-height .5s ease-in-out;
   display: flex;
   flex-direction: column;
+  padding: 0 60px;
   /* justify-content: space-evenly; */
 }
 .open {
   max-height: 3000px;
   opacity: 100;
   transition: opacity 2s ease-in-out, max-height 0.5s ease-in-out;
+}
+.dropdown-button {
+  transform: rotate(0deg);
+  transition: transform .25s linear;
+  margin-right: 20px;
+}
+.arrow-open {
+  transform: rotate(90deg);
+  transition: transform .25s linear;
 }
 .caption {
   width: 100%;
