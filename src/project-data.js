2e62886485603129c49afec2d451b2fa62f3cef0
Masaki Kleinkopf <masaki.kleinkopf@gmail.com>
2024-01-03 15:21:50 -0800

    change img srcs

--- src/project-data.js
+++ src/project-data.js
@@ -1,16 +1,16 @@
 export const projectData = [
   {title: "Live Nation", photos: [{
-    src:'/images/ln-6.png',
+    src:'/images/ln-6.webp',
     caption: 'Built new website for Live Nation Venues, utilizing Nuxt 3, TypeScript, Sanity CMS and SCSS'
   },
-  {src:'/images/ln-5.png', caption: 'Another one'},
-  {src:'/images/ln-10.png', caption: 'Another one'},
-  {src:'/images/ln-6.png', caption: 'Another one'},
-  {src:'/images/ln-4.png', caption: 'Another one'},
-  {src:'/images/ln-3.png', caption: 'Another one'},
-  {src:'/images/ln-7.png', caption: 'Another one'},
-  {src:'/images/ln-8.png', caption: 'Another one'},
-  {src:'/images/ln-9.png', caption: 'Another one'},
+  {src:'/images/ln-5.webp', caption: 'Another one'},
+  {src:'/images/ln-6.webp', caption: 'Another one'},
+  {src:'/images/ln-4.webp', caption: 'Another one'},
+  {src:'/images/ln-3.webp', caption: 'Another one'},
+  {src:'/images/ln-10.webp', caption: 'Another one'},
+  {src:'/images/ln-7.webp', caption: 'Another one'},
+  {src:'/images/ln-8.webp', caption: 'Another one'},
+  {src:'/images/ln-9.webp', caption: 'Another one'},
 
 ]
   },
